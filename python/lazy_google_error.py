ec346d1fcbe93bcae0b8d052aaadfaf89a7678f8
MPADlein <75044614+mpadlein@users.noreply.github.com>
2022-10-02 16:40:00 +0700

    Update lazy_google_error.py

--- python/lazy_google_error.py
+++ python/lazy_google_error.py
@@ -1,12 +1,13 @@
 import sys
 import traceback
 
+
 _OPEN_URL_IN_BROWSER = False
 def exc_handler(exc_type, exc, *args):
-    print("".join(traceback.format_exception(type(exc), exc, exc.__traceback__)))
-    q = f'python {type(exc).__name__} {exc} site:stackoverflow.com'
+    print("".join(traceback.format_exception(exc_type, exc, exc.__traceback__)))
+    q = f'python {exc_type.__name__}  {exc} site:stackoverflow.com'
+    q = q.replace(' ', '+')
     url = f'https://www.google.com/search?q={q}'
-    url = url.replace(' ', '%20')
 
     if _OPEN_URL_IN_BROWSER:
         import webbrowser
